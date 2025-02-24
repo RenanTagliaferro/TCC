aa502bec6c9e7271a80df97ae7735873966f40c9
RENAN <RenanTagliaferro2@gmail.com>
2025-02-24 08:59:52 -0300

    criando SLN

---
+++ BankTransferAPI/BankTransferAPI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BankTransferAPI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
