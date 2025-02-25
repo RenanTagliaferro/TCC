16dc4f8f8416328bbdcc38ad56315e4aa239c3fc
RENAN <RenanTagliaferro2@gmail.com>
2025-02-25 08:40:22 -0300

    add referencias a Bibliotecas externas

--- BankTransferAPI/BankTransferAPI/obj/Debug/net8.0/BankTransferAPI.AssemblyInfo.cs
+++ BankTransferAPI/BankTransferAPI/obj/Debug/net8.0/BankTransferAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankTransferAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa502bec6c9e7271a80df97ae7735873966f40c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79a5a86415d835c1d6c7c7dc006cd1247ab04522")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankTransferAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankTransferAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
